d54c0c19abef931a7362c3ef23a976643a279d28
sreerag-13 <sreeragsaji41@gmail.com>
2024-06-07 22:19:06 +0530

    route complete

--- src/App.js
+++ src/App.js
@@ -4,15 +4,23 @@ import AddLens from './components/AddLens';
 import DeleteLen from './components/DeleteLen';
 import SearchLens from './components/SearchLens';
 import ViewLen from './components/ViewLen';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <AddLens/>
-      <DeleteLen/>
-      <SearchLens/>
-      <ViewLen/>
-    </div>
+  
+    <BrowserRouter>
+    
+    <Routes>
+
+      <Route path='/' element={<AddLens/>}/>
+      <Route path='/SearchLens' element={<SearchLens/>}/>
+      <Route path='/DeleteLen' element={<DeleteLen/>}/>
+      <Route path='/ViewLen' element={<ViewLen/>}/>
+    </Routes>
+    
+    
+    </BrowserRouter>
   );
 }
 
--- src/components/NavBarlen.jsx
+++ src/components/NavBarlen.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const NavBarlen = () => {
   return (
@@ -6,23 +7,23 @@ const NavBarlen = () => {
 
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="#">LENSKART</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Add lens</a>
+          <Link class="nav-link active" aria-current="page" to="/">Add lens</Link>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Delete</a>
+          <Link class="nav-link" to="/DeleteLen">Delete</Link>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">search</a>
+          <Link class="nav-link" to="/SearchLens">search</Link>
         </li>
         <li class="nav-item">
-        <a class="nav-link" href="#">lens list</a>
+        <Link class="nav-link" to="/ViewLen">lens list</Link>
         </li>
       </ul>
     </div>
