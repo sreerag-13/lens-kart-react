f18071fc8e60bfa9c5c2139a0ed1b6b4c6539635
sreerag-13 <sreeragsaji41@gmail.com>
2024-06-07 21:23:57 +0530

    complete

---
+++ src/components/AddLens.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+
+const AddLens = () => {
+  return (
+    <div>
+
+    <div className="container">
+        <div className="row">
+            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12"></div>
+            <div className="row g-3">
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                    <label htmlFor="" className="form-label">Brand name</label>
+                    <input type="text" className="form-control" />
+                </div>
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                    <label htmlFor="" className="form-label">Category</label>
+                    <select name="" id="" className="form-control">
+                        <option value="">contact lens</option>
+                        <option value="">screen lens</option>
+                        <option value="">eye lens</option>
+                    </select>
+                </div>
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <label htmlFor="" className="form-label">Property </label>
+                    <input type="text" className="form-control" />
+                </div>
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                <label htmlFor="" className="form-label">Price </label>
+                <input type="text" className="form-control" />
+                </div>
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                <label htmlFor="" className="form-label">power</label>
+                <input type="text" className="form-control" />
+
+
+                </div>
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                    <button className="btn btn-success">add</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+  )
+}
+
+export default AddLens
---
+++ src/components/DeleteLen.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const DeleteLen = () => {
+  return (
+    <div>
+
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12"></div>
+                <label htmlFor="" className="form-label">Seacrch lens</label>
+                <input type="text" className="form-control"/>
+                <button className="btn btn-danger">Delete</button>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default DeleteLen
